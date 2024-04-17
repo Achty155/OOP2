77428e6fa4187e36bd909e1d937d2876e5bdbb86
Michal Kačinec <miso42850@gmail.com>
2024-04-17 12:13:07 +0200

    GUI pridany listener na combobox

--- src/Graphs.java
+++ src/Graphs.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Graphs {
     private JPanel panel1;
     private JComboBox comboBox1;
+    private JTextField textField1;
 
     public Graphs(State[] staty) {
    /* for(int i = 0; i < 50; i++){
@@ -19,11 +23,24 @@ public class Graphs {
         } //TODO protože přidávání itemů do boxu v konstruktoru nezobrazovalo žádné itemy, tak nejspíš
         //TODO bylo potřeba to dát až sem, aby byli již vytvořené a aktulizované
         panel1.add(comboBox1);
+        panel1.add(textField1);
         frame.setContentPane(panel1);
         frame.setTitle("Graphs");
         frame.setSize(600, 400);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
+
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox comboBox = (JComboBox) e.getSource();
+                String selectedState = (String) comboBox.getSelectedItem();
+                textField1.setText(selectedState);
+            }
+        });
+
+
+
     }
     public JPanel getPanel1() {
         return panel1;
@@ -36,4 +53,5 @@ public class Graphs {
     public void setComboBox1(JComboBox comboBox1) {
         this.comboBox1 = comboBox1;
     }
+
 }
