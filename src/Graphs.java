eefee09f369f3b1ed8b77a5c35f4d8dd5ffccffc
Michal Kačinec <miso42850@gmail.com>
2024-04-17 13:29:30 +0200

    GUI kus upravene gui na grafy

--- src/Graphs.java
+++ src/Graphs.java
@@ -5,26 +5,26 @@ import java.awt.event.ActionListener;
 
 public class Graphs {
     private JPanel panel1;
-    private JComboBox comboBox1;
+    private JComboBox<String> comboBox1;
     private JTextField textField1;
 
     public Graphs(State[] staty) {
-   /* for(int i = 0; i < 50; i++){
-        comboBox1.addItem(staty[i].getName());
+        panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+        comboBox1 = new JComboBox<>();
+        textField1 = new JTextField(20);
 
-    }*/
-    }
-    public void graphsUI(State[] staty){
-        JFrame frame = new JFrame("Graphs");
-        panel1 = new JPanel(); //TODO Panel nebyl vytvořen a nadefinován, tudiž to "c" is null byl panel
-        for(int i = 0; i < 50; i++){
+        for (int i = 0; i < 50; i++) {
             comboBox1.addItem(staty[i].getName());
+        }
 
-        } //TODO protože přidávání itemů do boxu v konstruktoru nezobrazovalo žádné itemy, tak nejspíš
-        //TODO bylo potřeba to dát až sem, aby byli již vytvořené a aktulizované
         panel1.add(comboBox1);
         panel1.add(textField1);
-        frame.setContentPane(panel1);
+    }
+
+    public void graphsUI(State[] staty){
+        JFrame frame = new JFrame("Graphs");
+        frame.setContentPane(panel1); // Set the content pane to the already initialized panel
         frame.setTitle("Graphs");
         frame.setSize(600, 400);
         frame.setVisible(true);
@@ -38,20 +38,17 @@ public class Graphs {
                 textField1.setText(selectedState);
             }
         });
-
-
-
     }
+
     public JPanel getPanel1() {
         return panel1;
     }
 
-    public JComboBox getComboBox1() {
+    public JComboBox<String> getComboBox1() {
         return comboBox1;
     }
 
-    public void setComboBox1(JComboBox comboBox1) {
+    public void setComboBox1(JComboBox<String> comboBox1) {
         this.comboBox1 = comboBox1;
     }
-
 }
