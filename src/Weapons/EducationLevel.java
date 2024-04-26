8449ed684d4430f5f86bc068eb645e9c76e9706b
miso <miso42850@gmail.com>
2024-04-26 12:39:12 +0200

    Weapons - hotovy javadoc

--- src/Weapons/EducationLevel.java
+++ src/Weapons/EducationLevel.java
@@ -2,19 +2,32 @@ package Weapons;
 
 import java.util.Random;
 
+/**
+ * The EducationLevel class represents an object indicating the education level, which is randomly generated.
+ */
 public class EducationLevel {
 
     private int EdLevel;
 
+    /**
+     * Constructs a new EducationLevel object and calculates its education level.
+     */
     public EducationLevel(){
-            calculateEdLevel();
+        calculateEdLevel();
     }
 
+    /**
+     * Calculates the education level randomly.
+     */
     protected void calculateEdLevel(){
         Random rand = new Random();
         this.EdLevel = rand.nextInt(2); // 0 or 1
     }
 
+    /**
+     * Retrieves the education level.
+     * @return The education level.
+     */
     public int getEdLevel() {
         return EdLevel;
     }
